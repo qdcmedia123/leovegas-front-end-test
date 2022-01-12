dd5c1f3481a015a81640341480246738b6e7666d
Bharat Shah <akashche72@gmail.com>
2022-01-12 16:20:46 +0400

    Pagination component has been developed

--- src/components/movies/index.tsx
+++ src/components/movies/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Card from 'components/common/Card';
 
-const Movies = () => {
+const Movies = (movies: any) => {
+    
     return (
-        
-            <Card/>
-        
+        <section className="movies-sections">
+            <Card />
+            <div className="pagination">
+                <ul className="pagination-3">
+                    <li className="page-number prev"><a href="!#">&laquo;</a></li>
+                    <li className="page-number active"><a href="!#">1</a></li>
+                    <li className="page-number"><a href="!#">2</a></li>
+                    <li className="page-number"><a href="!#">3</a></li>
+                    <li className="page-number"><a href="!#">4</a></li>
+                    <li className="page-number"><a href="!#">5</a></li>
+                    <li className="page-number"><a href="!#">6</a></li>
+                    <li className="page-number prev"><a href="!#">&raquo;</a></li>
+                </ul>
+            </div>
+        </section>
+
     );
 };
 
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useTypedSelector } from 'hooks/use-typed-selector';
 import Movies from 'components/movies';
+import LoadingSVG from 'assets/img/loading.svg';
 interface Section9Props {
 }
 export const Search: React.FC<Section9Props> = ({ }) => {
+    // Send http request from here 
+    // Pass the movies to movies props 
     return (
         <><div className="section9">
             <div className="search">
@@ -11,7 +14,19 @@ export const Search: React.FC<Section9Props> = ({ }) => {
                 <button className="search__btn">Search</button>
             </div>
         </div>
+
         <Movies/>
+        <div className="main-message">
+            {/* <div className="no-data">
+                No data found
+            </div>  */}
+            {/* <div className="loading">
+                <img src={LoadingSVG} alt="" />
+            </div> */}
+
+        </div>
+
+       
         </>
         
         );
